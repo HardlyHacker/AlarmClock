66fb8f97501dca31bb72b49d0a687889e9db93fb
HardlyHacker <vedakomkar@yahoo.in>
2018-05-14 23:26:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/omkar/alarmclock/AlarmAdapter.java
@@ -0,0 +1,22 @@
+
+
+package com.razormist.simplealarmapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+
+/**
+ * Created by Omkar on 5/5/2018.
+ */
+
+public class AlarmAdapter extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.start();
+    }
+}
